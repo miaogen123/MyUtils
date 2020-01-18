cc7afd912701a65171b57408c55329ccbac81eca
ubuntu <ubuntu@localhost.localdomain>
2020-01-18 23:24:39 +0800

    modify format

--- cpp/fileOp.h
+++ cpp/fileOp.h
@@ -1,4 +1,6 @@
 #pragma once
 #include<string>
+#include<vector>
 
 std::string getAllOfFile(std::string filename);
+void SplitString(const std::string& s, std::vector<std::string>& v, const std::string& c);
--- cpp/timeRela.cpp
+++ cpp/timeRela.cpp
@@ -13,10 +13,10 @@ std::string getCurrentOfFormat(std::string format) {
     auto t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::stringstream  ss;
 	try {
-		ss<< std::put_time(std::localtime(&t),format) ;
+		ss<< std::put_time(std::localtime(&t), format.c_str());
 	}
 	catch (std::exception& e) {
-		throw std::runtime_error("format error"):
+		throw std::runtime_error("format error");
 	}
     return std::string(ss.str());
 }
